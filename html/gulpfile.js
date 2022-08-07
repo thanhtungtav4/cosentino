371d2afaa94b3469fc5c126a7505e7fb3c3dc410
van nguyen <vannguyengithub@gmail.com>
2022-08-07 19:01:49 +0700

    index

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const del = require('del');
