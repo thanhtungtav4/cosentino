1e82c606c14cb589dfa91a4ce66a576258a08ecc
tungnt <nguyenthanhtung74119@gmail.com>
2022-08-27 17:48:58 +0700

    tungnt

---
+++ wp-content/themes/twentytwenty-child/header.php
@@ -0,0 +1,402 @@
+<?php
+
+/**
+ * Header file for the Twenty Twenty WordPress default theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Top</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/assets/css/style.css">
+    <link rel="preload" as="style" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    </noscript>
+	<?php wp_head(); ?>
+  </head>
+  <body>
+    <div class="container">
+      <header class="c-header">
+        <div class="c-header_top">
+          <div class="l-container">
+            <ul class="c-header_nav">
+              <li> <a href="#">Showroom</a></li>
+              <li> <a href="#">Tin Tức</a></li>
+            </ul>
+            <div class="c-header_logo">
+				<a href="/">
+					<picture>
+					<source srcset="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.svg" type="image/svg"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.svg" data-src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.svg" alt="logo">
+					</picture>
+				</a>
+			</div>
+          </div>
+        </div>
+        <div class="c-header_bottom">
+          <div class="l-container c-header_inner">
+            <ul class="c-header_menu">
+              <li class="c-menu c-menu_dropdown bg-full"><a href="#">Brands</a>
+                <div class="c-menu_sub">
+                  <div class="c-menu_subinner">
+                    <ul> 
+                      <li class="c-menu c-menu_dropdown show" id="silestone"><a href="#">Silestone</a>
+                        <div class="c-menu_sub">
+                          <div class="l-container col-3">
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen</p></a></dd>
+                            </dl>
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen</p></a></dd>
+                            </dl>
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen   </p></a></dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </li>
+                      <li class="c-menu c-menu_dropdown hide" id="dekstone"><a href="#">Dekstone</a>
+                        <div class="c-menu_sub">
+                          <div class="l-container col-3">
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen1111</p></a></dd>
+                            </dl>
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen</p></a></dd>
+                            </dl>
+                            <dl>
+                              <dd><a href="#">
+                                  <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                                  <p class="ttl">3d-kitchen                                                      </p></a></dd>
+                            </dl>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Spaces</a>
+                <div class="c-menu_sub">
+                  <div class="c-menu_subinner">
+                    <div class="l-container col-4">
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">Kitchen</p></a></dd>
+                        <dd> <a href="#">Kitchen Countertops     </a></dd>
+                        <dd> <a href="#">Kitchen Claddings   </a></dd>
+                        <dd> <a href="#">Kitchen Flooring       </a></dd>
+                      </dl>
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">BATHROOMS</p></a></dd>
+                        <dd> <a href="#">Bathroom Claddings</a></dd>
+                        <dd> <a href="#">Bathroom flooring</a></dd>
+                        <dd> <a href="#">Bathroom Remodelings   </a></dd>
+                        <dd> <a href="#">Bathroom countertops                       </a></dd>
+                      </dl>
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">Living Rooms</p></a></dd>
+                        <dd> <a href="#">Furniture   </a></dd>
+                        <dd> <a href="#">Floor Coverings</a></dd>
+                        <dd> <a href="#">Claddings</a></dd>
+                      </dl>
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">Outdoor</p></a></dd>
+                        <dd> <a href="#">Claddings   </a></dd>
+                        <dd> <a href="#">Outside use kitchens </a></dd>
+                        <dd> <a href="#">Swimming pool</a></dd>
+                        <dd> <a href="#">Outside use kitchens                       </a></dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Inspiration</a>
+                <div class="c-menu_sub">
+                  <div class="c-menu_subinner">
+                    <div class="l-container col-3">
+                      <dl>
+                        <dt>Showroom</dt>
+                        <dd><a href="#">
+                            <picture>
+                              <source srcset="assets/images/img_menu.webp" type="image/webp"><img class="lazyload" src="assets/images/img_menu.png" data-src="assets/images/img_menu.png" alt="img_menu" loading="lazy" width="437" height="200">
+                            </picture></a></dd>
+                      </dl>
+                      <dl>
+                        <dt>Đo mắt miễn phí
+                          <dd><a href="#">
+                              <picture>
+                                <source srcset="assets/images/img_menu.webp" type="image/webp"><img class="lazyload" src="assets/images/img_menu.png" data-src="assets/images/img_menu.png" alt="img_menu" loading="lazy" width="437" height="200">
+                              </picture></a></dd>
+                        </dt>
+                      </dl>
+                      <dl>
+                        <dt>Tư ván online
+                          <dd><a href="#">
+                              <picture>
+                                <source srcset="assets/images/img_menu.webp" type="image/webp"><img class="lazyload" src="assets/images/img_menu.png" data-src="assets/images/img_menu.png" alt="img_menu" loading="lazy" width="437" height="200">
+                              </picture></a></dd>
+                        </dt>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Visualizers</a>
+                <div class="c-menu_sub">
+                  <div class="c-menu_subinner">
+                    <div class="l-container col-3">
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">3d-kitchen</p></a></dd>
+                      </dl>
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">3d-kitchen</p></a></dd>
+                      </dl>
+                      <dl>
+                        <dd><a href="#">
+                            <picture><img class="lazyload" src="assets/images/online_visualizer.jpg" data-src="assets/images/online_visualizer.jpg" alt="img_menu" loading="lazy" width="437" height="200"></picture>
+                            <p class="ttl">3d-kitchen                      </p></a></dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Cosentino City</a></li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Where to buy       </a></li>
+              <li class="c-menu c-menu_dropdown"><a href="#">Professionals           </a></li>
+            </ul>
+            <ul class="c-header_icon">
+              <li class="navbar only-sp" onclick="toggleMenu()">
+                <svg width="28" height="18" viewBox="0 0 28 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 3V0H27.5V3H0.5ZM0.5 10.5H27.5V7.5H0.5V10.5ZM0.5 18H27.5V15H0.5V18Z" fill="#ffffff"></path>
+                </svg>
+              </li>
+              <li class="search c-menu_dropdown only-sp"><a class="icon_inner" href="#">
+                  <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" fill="#ffffff" d="M22.4475 21.3826H23.6325L31.1175 28.8826L28.8825 31.1176L21.3825 23.6326V22.4476L20.9775 22.0276C19.2675 23.4976 17.0475 24.3826 14.6325 24.3826C9.24751 24.3826 4.88251 20.0176 4.88251 14.6326C4.88251 9.24757 9.24751 4.88257 14.6325 4.88257C20.0175 4.88257 24.3825 9.24757 24.3825 14.6326C24.3825 17.0476 23.4975 19.2676 22.0275 20.9776L22.4475 21.3826ZM7.88251 14.6326C7.88251 18.3676 10.8975 21.3826 14.6325 21.3826C18.3675 21.3826 21.3825 18.3676 21.3825 14.6326C21.3825 10.8976 18.3675 7.88257 14.6325 7.88257C10.8975 7.88257 7.88251 10.8976 7.88251 14.6326Z"></path>
+                  </svg></a>
+                <div class="c-menu_sub">
+                  <div class="c-menu_subinner">
+                    <form action="/" method="get">
+                      <input type="text" name="s" id="search" placeholder="Search"><i class="gg-search"></i>
+                    </form>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+      <main class="l-main">
+
+
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+
+	<head>
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<?php wp_head(); ?>
+
+	</head>
+
+	<body <?php body_class(); ?>>
+
+		<?php
+		wp_body_open();
+		?>
+
+		<header id="site-header" class="header-footer-group">
+
+			<div class="header-inner section-inner">
+
+				<div class="header-titles-wrapper">
+
+					<?php
+
+					// Check whether the header search is activated in the customizer.
+					$enable_header_search = get_theme_mod( 'enable_header_search', true );
+
+					if ( true === $enable_header_search ) {
+
+						?>
+
+						<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
+							<span class="toggle-inner">
+								<span class="toggle-icon">
+									<?php twentytwenty_the_theme_svg( 'search' ); ?>
+								</span>
+								<span class="toggle-text"><?php _ex( 'Search', 'toggle text', 'twentytwenty' ); ?></span>
+							</span>
+						</button><!-- .search-toggle -->
+
+					<?php } ?>
+
+					<div class="header-titles">
+
+						<?php
+							// Site title or logo.
+							twentytwenty_site_logo();
+
+							// Site description.
+							twentytwenty_site_description();
+						?>
+
+					</div><!-- .header-titles -->
+
+					<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+						<span class="toggle-inner">
+							<span class="toggle-icon">
+								<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
+							</span>
+							<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
+						</span>
+					</button><!-- .nav-toggle -->
+
+				</div><!-- .header-titles-wrapper -->
+
+				<div class="header-navigation-wrapper">
+
+					<?php
+					if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
+						?>
+
+							<nav class="primary-menu-wrapper" aria-label="<?php echo esc_attr_x( 'Horizontal', 'menu', 'twentytwenty' ); ?>">
+
+								<ul class="primary-menu reset-list-style">
+
+								<?php
+								if ( has_nav_menu( 'primary' ) ) {
+
+									wp_nav_menu(
+										array(
+											'container'  => '',
+											'items_wrap' => '%3$s',
+											'theme_location' => 'primary',
+										)
+									);
+
+								} elseif ( ! has_nav_menu( 'expanded' ) ) {
+
+									wp_list_pages(
+										array(
+											'match_menu_classes' => true,
+											'show_sub_menu_icons' => true,
+											'title_li' => false,
+											'walker'   => new TwentyTwenty_Walker_Page(),
+										)
+									);
+
+								}
+								?>
+
+								</ul>
+
+							</nav><!-- .primary-menu-wrapper -->
+
+						<?php
+					}
+
+					if ( true === $enable_header_search || has_nav_menu( 'expanded' ) ) {
+						?>
+
+						<div class="header-toggles hide-no-js">
+
+						<?php
+						if ( has_nav_menu( 'expanded' ) ) {
+							?>
+
+							<div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
+
+								<button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+									<span class="toggle-inner">
+										<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
+										<span class="toggle-icon">
+											<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
+										</span>
+									</span>
+								</button><!-- .nav-toggle -->
+
+							</div><!-- .nav-toggle-wrapper -->
+
+							<?php
+						}
+
+						if ( true === $enable_header_search ) {
+							?>
+
+							<div class="toggle-wrapper search-toggle-wrapper">
+
+								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
+									<span class="toggle-inner">
+										<?php twentytwenty_the_theme_svg( 'search' ); ?>
+										<span class="toggle-text"><?php _ex( 'Search', 'toggle text', 'twentytwenty' ); ?></span>
+									</span>
+								</button><!-- .search-toggle -->
+
+							</div>
+
+							<?php
+						}
+						?>
+
+						</div><!-- .header-toggles -->
+						<?php
+					}
+					?>
+
+				</div><!-- .header-navigation-wrapper -->
+
+			</div><!-- .header-inner -->
+
+			<?php
+			// Output the search modal (if it is activated in the customizer).
+			if ( true === $enable_header_search ) {
+				get_template_part( 'template-parts/modal-search' );
+			}
+			?>
+
+		</header><!-- #site-header -->
+
+		<?php
+		// Output the menu modal.
+		get_template_part( 'template-parts/modal-menu' );
